b5be2387f9c32c40c2b25591095354ce55a98b50
Pranav Vinod <75975919+pr-glitch@users.noreply.github.com>
2025-07-02 13:46:30 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+// Just some dummy interactivity
+console.log("Welcome to Dummy Dev's portfolio!");
+
+document.addEventListener("DOMContentLoaded", () => {
+  alert("This site does absolutely nothing useful. Enjoy!");
+});
